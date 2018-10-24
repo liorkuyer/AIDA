46a194c4ab404c3559fa2cf0d1db134bbbf10b1f
Lior Kuyer <liorkuyer@gmail.com>
2018-10-24 12:45:24 -0700

    fix pencil class

--- src/components/editor/tools/simple/Pencil.vue
+++ src/components/editor/tools/simple/Pencil.vue
@@ -15,6 +15,7 @@
         <i
           :class="{
             'fa': true,
+            'fa-pencil': true,
             'fa-pencil-alt': true,
             'faIcons': !active,
             'faIconsActive': active
